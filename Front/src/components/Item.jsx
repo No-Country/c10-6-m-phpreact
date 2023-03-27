fb875b5244a13c819b82630f4fa468fa9044a9d4
Tomas Juarez <mokajua@gmail.com>
2023-03-27 14:54:24 -0300

    .

--- Front/src/components/Item.jsx
+++ Front/src/components/Item.jsx
@@ -8,27 +8,27 @@ export const Item = ({nombre,id,descripcion,precio})=>{
 
     return(
         <>
-        <Card  sx={{ maxWidth: 345 , margin:"2%",  }}>
-                <CardMedia
-                sx={{ height: 140 }}
-                title={nombre}
-                image=""
-                />
-                <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                {nombre}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {descripcion}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                   Precio: {precio}
-                </Typography>
-                </CardContent>
-                <CardActions>
-                    <NavLink>ver plato </NavLink>
-                </CardActions>
-        </Card>
+            <Card  sx={{ maxWidth: 345 , margin:"2%",  }}>
+                    <CardMedia
+                    sx={{ height: 140 }}
+                    title={nombre}
+                    image=""
+                    />
+                    <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                    {nombre}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {descripcion}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                    Precio: {precio}
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <NavLink>ver plato </NavLink>
+                    </CardActions>
+            </Card>
         </>
     )
 }
