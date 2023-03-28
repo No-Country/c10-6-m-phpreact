7208796ea20a00208e026ee291e9cbf4e02d313c
raffarraffa <50971821+raffarraffa@users.noreply.github.com>
2023-03-28 15:24:05 -0300

    Se corrigieron tablas

--- nocountry.sql
+++ nocountry.sql
@@ -6,66 +6,69 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE IF NOT EXISTS `nocountry` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `nocountry`;
+CREATE DATABASE IF NOT EXISTS `resto_nocountry` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `resto_nocountry`;
+
+DROP TABLE IF EXISTS `categoria`;
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `categoria` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `descripcion` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
+  `creado` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 DROP TABLE IF EXISTS `descuento`;
 CREATE TABLE IF NOT EXISTS `descuento` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `descuento` int NOT NULL,
+  `id_descuento` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `descuento` int UNSIGNED NOT NULL,
   `descripcion` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `fecha_caducidad` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `activo` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_descuento`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 DROP TABLE IF EXISTS `estado`;
 CREATE TABLE IF NOT EXISTS `estado` (
   `id` int NOT NULL AUTO_INCREMENT,
   `descripcion` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='estado pedidos';
-
-DROP TABLE IF EXISTS `log`;
-CREATE TABLE IF NOT EXISTS `log` (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `type` tinyint UNSIGNED NOT NULL COMMENT '0:logout\r\n1:login',
-  `creado` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='control login user';
-
-DROP TABLE IF EXISTS `materias_primas`;
-CREATE TABLE IF NOT EXISTS `materias_primas` (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nombre` tinyint UNSIGNED NOT NULL,
-  `creado` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='producto opcional';
 
 DROP TABLE IF EXISTS `mesas`;
 CREATE TABLE IF NOT EXISTS `mesas` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `mesa` int NOT NULL,
+  `estado_mesa` tinyint UNSIGNED NOT NULL COMMENT '0-libre / cerrada /cobrada\r\n1- iniciada\r\n2 pedido mozo/ cobro\r\n',
   `qr` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 DROP TABLE IF EXISTS `pedidos`;
 CREATE TABLE IF NOT EXISTS `pedidos` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
   `mesa` tinyint UNSIGNED NOT NULL,
-  `detalle` int NOT NULL,
-  `estado` int NOT NULL,
+  `detalle` json NOT NULL,
+  `importe_total` decimal(9,2) UNSIGNED NOT NULL,
+  `descuento` tinyint UNSIGNED NOT NULL,
+  `estado` int UNSIGNED NOT NULL COMMENT '0 - cerrado-cobrado\r\n 1 activo-sin pagar\r\n 2 - activo-en-proceso-de-pago',
+  `cliente` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
   `creado` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `cerrado` datetime NOT NULL,
+  `observacion` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 DROP TABLE IF EXISTS `productos`;
 CREATE TABLE IF NOT EXISTS `productos` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nombre` tinyint UNSIGNED NOT NULL,
-  `materias_primas` int NOT NULL,
+  `nombre_producto` tinyint UNSIGNED NOT NULL,
+  `descripcion` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
   `precio` int NOT NULL,
   `descuento` int NOT NULL,
   `creado` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `categoria` tinyint UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -88,6 +91,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `creado` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS `user_control`;
+CREATE TABLE IF NOT EXISTS `user_control` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `tipo` tinyint UNSIGNED NOT NULL COMMENT '0:logout\r\n1:login',
+  `creado` datetime NOT NULL,
+  `user` int UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='control login user';
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
