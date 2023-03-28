abd8bce37134752a30ce85ea039728198c6ab40f
fduples <84922163+fduples@users.noreply.github.com>
2023-03-28 15:25:07 -0300

    inicio de construccion de enrutadores y agregad de .htaccess

---
+++ FedeDu/Control/rutas.php
@@ -0,0 +1,19 @@
+<?php
+namespace Control;
+
+class Ruta
+{
+    private $rutas = [
+        'menu' => $menu, //nos va a redirigir a una funcion que va a ser la que enruta
+        'comanda' => $comanda, //enrutador comada cocina
+        'mozo' => $servicio, //enrutador atencio en mesa
+        'adminitrador' => $adminitrador, //Enrutador cobro
+        'error' => $error, //Enrutadora errores
+    ];
+
+}
+
+
+
+
+?>
