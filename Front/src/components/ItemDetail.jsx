f682e8595f4fdc02b2bee540a07a3f427dc77e0d
Tomas Juarez <mokajua@gmail.com>
2023-04-01 17:45:26 -0300

    redux state

--- Front/src/components/ItemDetail.jsx
+++ Front/src/components/ItemDetail.jsx
@@ -8,17 +8,17 @@ export const ItemDetail = ({nombre,id,descripcion,precio})=>{
         return(
             <>  
                 <Container>
-                <br></br>
-                <br></br>
-                <br></br>
-                <h1>{nombre} </h1>
-                <h2>{descripcion}</h2>
-                <ul>
-                    <li>ingrediente</li>
-                    <li>ingrediente</li>
-                    <li>{precio}</li>
-                </ul>
-                <button>agregar pedido</button>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <h1>{nombre} </h1>
+                    <h2>{descripcion}</h2>
+                    <ul>
+                        <li>ingrediente</li>
+                        <li>ingrediente</li>
+                        <li>{precio}</li>
+                    </ul>
+                    <button>agregar pedido</button>
                 </Container>           
             </>
         )
