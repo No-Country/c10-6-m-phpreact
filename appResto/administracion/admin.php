a412b17f4e83cfd684a2b93a8e9b5a01e78231d1
fduples <84922163+fduples@users.noreply.github.com>
2023-04-18 11:27:24 -0300

    final mozo y admin con base nueva

--- appResto/administracion/admin.php
+++ appResto/administracion/admin.php
@@ -9,14 +9,24 @@
     <title>Administración</title>
 </head>
 <body data-bs-theme="dark">
-    
-    <div class="container">
-        <div>
-            <h1>Administración</h1>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <div>
+                <h1>Administración</h1>
+            </div>
+            <div>
+                <a href="mesas/publico/admin_mesas.php" class="btn btn-primary btn-lg">
+                <img src="img/mesas.png" alt="mesas" class="imgBotones">
+                Mesas
+                </a>
+                <a href="productos/vistas/mostrar_productos.php" class="btn btn-primary btn-lg">
+                <img src="img/productos.png" alt="mesas" class="imgBotones">
+                Productos
+                </a>
+            </div>
         </div>
-        <a href="mesas/publico/admin_mesas.php" class="btn btn-primary btn-lg">Mesas</a>
-        <a href="productos/vistas/mostrar_productos.php" class="btn btn-primary btn-lg">Productos</a>
-    </div>
+    </nav>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
     </script>
