f1fc19503d41b27a43913f4a579d0bb5d084e82c
Fade-tor <72475945+Fade-tor@users.noreply.github.com>
2023-04-20 00:38:38 -0300

    carrito a punto, falta query

--- appResto/cliente/publico/index_cliente.php
+++ appResto/cliente/publico/index_cliente.php
@@ -1,6 +1,6 @@
 <?php 
-require_once '../lib/clienteController.php';
-$bd = new ClienteController;
+require_once '../lib/conection.php';
+$bd = new conection;
 $productos = $bd->mostrar_productos();
 ?>
 <!DOCTYPE html>
@@ -8,7 +8,7 @@ $productos = $bd->mostrar_productos();
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Lista de Productos</title>
+	<title>Carta BOCATTO</title>
 	<link rel="stylesheet" href="css/estilosCliente.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
@@ -16,15 +16,20 @@ $productos = $bd->mostrar_productos();
 	<div class="container">
 	<nav class="navbar navbar-expand-lg bg-body-tertiary">
 		<div class="container-fluid">
+            <h1>BOCATTO Restó</h1>
 		</div>
 	</nav>
 		<div>
 		<div class="card-deck">
-		<form action="../lib/clienteController.php" method="POST">
+		<form action="../lib/clienteController.php?accion=agregar" method="POST">
+        <label for="id_mesa">Ingrese su número de mesa</label>
+        <input type="number" name="id_mesa" id="id_mesa" required>
+
     <?php foreach ($productos as $producto): ?>
+        <form action="../lib/clienteController.php?accion=agregar" method="POST">
         <div class="card">
             <!-- Campo para el checkbox -->
-            <input type="checkbox" name="producto" value="<?php echo $producto["id"]; ?>">
+            <input type="hidden" name="producto_id" value="<?php echo $producto["id"]; ?>" class="m-1">
             <!-- Imagen y detalles del producto -->
             <img src="<?php echo $producto['imagen']; ?>" class="card-img-top" alt="Imagen del producto">
             <div class="card-body">
@@ -32,18 +37,22 @@ $productos = $bd->mostrar_productos();
                 <p class="card-text"><?php echo $producto['descripcion']; ?></p>
                 <p class="card-text">$<?php echo $producto['precio']; ?></p>
                 <!-- Campo para las observaciones -->
-                <input type="text" name="observaciones" class="form-control mb-2" placeholder="Observaciones">
-				<input type="hidden" name="precio" value="<?php echo $producto['precio']; ?>">
+                <div class="form-group m-2">
+                    <label for="input-number">Cantidad:</label>
+                    <input type="number" name="cantidad" class="form-control" id="input-number" min="1" max="10" step="1" value="1">
+                </div>
+                <div class="m-2">
+                <input type="text" name="observaciones" class="form-control" placeholder="Observaciones">
+				<input type="hidden" name="precio" value="<?php echo $producto['precio']; ?>" class="m-2">
+                </div>
             </div>
         </div>
     <?php endforeach; ?>
 
     <!-- Campo para el ID del cliente -->
-	<label for="id_mesa">Ingrese su número de mesa</label>
-    <input type="number" name="id_mesa" id="id_mesa">
-
+	
     <!-- Botón de enviar -->
-    <button type="submit" name="enviar_pedido" class="btn btn-primary">Enviar Pedido</button>
+    
 </form>
 </div>
 
